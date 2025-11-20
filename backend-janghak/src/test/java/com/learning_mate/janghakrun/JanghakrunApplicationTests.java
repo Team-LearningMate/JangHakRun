da475e0dd35a5072326f0ad526a1411199b990b0
yeahyeon <yhyh1216@gmail.com>
2025-11-20 23:25:40 +0900

    [CHORE] init spring boot project

---
+++ backend-janghak/src/test/java/com/learning_mate/janghakrun/JanghakrunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning_mate.janghakrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanghakrunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
