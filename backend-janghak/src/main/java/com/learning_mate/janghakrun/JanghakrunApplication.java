4cba3b3d6cef62fbbff320c98fa9ab5e6590c6be
yeahyeon <yhyh1216@gmail.com>
2025-11-21 09:02:28 +0900

    [CHORE] init spring boot project

---
+++ backend-janghak/src/main/java/com/learning_mate/janghakrun/JanghakrunApplication.java
@@ -0,0 +1,13 @@
+package com.learning_mate.janghakrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanghakrunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanghakrunApplication.class, args);
+	}
+
+}
