d3d23fb3c5c0324ffca3fc3d7b7ac2180440d496
yeahyeon <yhyh1216@gmail.com>
2025-11-22 01:10:12 +0900

    [FEAT] GlobalExceptionHandler 추가 및 exception 위치 이동 (close #11)

---
+++ backend-janghak/src/main/java/com/learning_mate/janghakrun/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.learning_mate.janghakrun.global.error;
+
+import com.learning_mate.janghakrun.global.exception.BusinessException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 비즈니스 로직 예외 처리
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        ErrorCode errorCode = e.getErrorCode();
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponse.of(errorCode));
+    }
+
+    // 그 외 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(BusinessException e) {
+        ErrorCode errorCode = e.getErrorCode();
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponse.of(errorCode));
+    }
+
+}
